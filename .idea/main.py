f1c5d4b477d204dd8af3e7e3d64558b39338e279
Denis <77mr.bro77@gmail.com>
2019-03-07 11:03:03 +0500

    Initial commit

---
+++ .idea/main.py
@@ -0,0 +1,10 @@
+import time
+
+def printLetters(str):
+    while True:
+        for i in str:
+            print(i)
+            time.sleep(0.3)
+        print("\n")
+
+printLetters("Дима Петух".upper())
