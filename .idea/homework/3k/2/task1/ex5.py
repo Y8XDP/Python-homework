222f443c49ba449034d23175e01fd7a243af55fe
Denis <77mr.bro77@gmail.com>
2019-03-13 14:29:55 +0500

    1 homework

--- .idea/homework/3k/2/task1/ex5.py
+++ .idea/homework/3k/2/task1/ex5.py
@@ -2,13 +2,8 @@ mass = [1, 3, 4, 5, 9, 10, 2, -6, -9, 90, 1000, 80, -24]
 
 i = 0;
 
-while i < int(len(mass) - 1):
-    m = int(i)
-    j = int(i+1)
-    while j < int(len(mass)):
-        if int(mass[j]) < int(mass[i]):
-            m = int(j)
-        j+=1
-    mass[i], mass[m] = mass[m], mass[i]
-    i+=1
+for i in range(len(mass)):
+    for j in range(len(mass)):
+        if mass[i] < mass[j]: mass[i], mass[j] = mass[j], mass[i]
+
 print(mass)
